40ce4fd9f29e96d9810fe2b941b327b28fb89633
Emmanue <kioxmanu7@gmail.com>
2023-10-29 03:22:22 +0300

    delete functionality added

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
   const toggleTheme = () => {
     setSelectedTheme(selectedTheme === 'light' ? 'dark' : 'light');
   };
+
   useEffect(() => {
     fetch('http://localhost:3000/transactions')
       .then((response) => {
@@ -36,69 +37,74 @@ const Home = () => {
     setSortBy('amount');
   };
 
+  const deleteTransaction = (id) => {
+    const updatedTransactions = transactions.filter((transaction) => transaction.id !== id);
+    setTransactions(updatedTransactions);
+  };
+
   const totalBalance = transactions.reduce((total, transaction) => {
     return total + parseFloat(transaction.amount);
   }, 0);
 
   return (
-    <div id='all' className='flex gap-5 px-4   items-center mx-auto content-center'>
-      
-
-      
-
-      <div className="table  " style={{ maxHeight: '900px', overflowY: 'auto' }}>
-      <input
-        className='border-blue-700  border-2 rounded-xl shadow border-solid py-5 px-8 lg:w-[50%] my-[50px]'
-        type="text"
-        placeholder="Search by description"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <select
-        value={selectedCategory}
-        onChange={(e) => setSelectedCategory(e.target.value)}
-        className='border-blue-700 border-2 rounded-xl mx-3 shadow border-solid  py-5 px-8 lg:w-[35%] my-[50px]'
-      >
-        
-  <option value="">All Categories</option>
-  <option value="Food">Food</option>
-  <option value="Income">Income</option>
-  <option value="Fashion">Fashion</option>
-  <option value="Gift">Gift</option>
-  <option value="Transportation">Transportation</option>
-  <option value="Entertainment">Entertainment</option>
-  <option value="Housing">Housing</option>
-      </select>
-        <table className='h-[auto] bg-red-300'>
+    <div id="all" className="flex gap-5 px-4 items-center mx-auto content-center">
+      <div className="table" style={{ maxHeight: '900px', overflowY: 'auto' }}>
+        <input
+          className="border-blue-700 border-2 rounded-xl shadow border-solid py-5 px-8 lg:w-[50%] my-[50px]"
+          type="text"
+          placeholder="Search by description"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <select
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          className="border-blue-700 border-2 rounded-xl mx-3 shadow border-solid py-5 px-8 lg:w-[35%] my-[50px]"
+        >
+          <option value="">All Categories</option>
+          <option value="Food">Food</option>
+          <option value="Income">Income</option>
+          <option value="Fashion">Fashion</option>
+          <option value="Gift">Gift</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Housing">Housing</option>
+        </select>
+        <table className="h-[auto] bg-red-300">
           <thead>
-            <tr className='text-purple-700'>
+            <tr className="text-purple-700">
               <th>Date</th>
               <th>Description</th>
               <th>Category</th>
               <th onClick={sortTransactionsByAmount} style={{ cursor: 'pointer' }}>
                 Amount
               </th>
+              <th>Actions</th>
             </tr>
           </thead>
 
-          <tbody className=''>
+          <tbody className="">
             {transactions
               .filter((transaction) =>
                 transaction.description.toLowerCase().includes(searchTerm.toLowerCase()) &&
                 (selectedCategory === '' || transaction.category === selectedCategory)
               )
               .map((transaction) => (
-                <tr className='h-full' key={transaction.id}>
+                <tr className="h-full" key={transaction.id}>
                   <td>{transaction.date}</td>
                   <td>{transaction.description}</td>
                   <td>{transaction.category}</td>
                   <td>{transaction.amount}</td>
+                  <td>
+                    <button onClick={() => deleteTransaction(transaction.id)} className="bg-red-500 text-white px-2 py-1 rounded">
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
           </tbody>
         </table>
       </div>
-
       <div>Total Balance: ${totalBalance.toFixed(2)}</div>
     </div>
   );
